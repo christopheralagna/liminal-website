51b9e721d368001a09f21378133c430ce72c7f0a
Christopher Alagna <christopheralagna1@gmail.com>
2021-01-28 17:57:52 -0700

    fixed navbar issues including vertical-sizing and javascript-related errors. Also updated navbar and banner sizing media query breakpoints. Also evened flower-img footer margins for each media query breakpoint.

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,22 @@ const navBar = document.querySelector(".nav-bar");
 const links = document.querySelector(".links");
 const verticalLinks = document.querySelector('.vertical-links');
 const section = document.querySelector('section');
+const barsIconSizeQuery = window.matchMedia('(max-width: 500px)');
+const verticalNavQuery = window.matchMedia('(min-width: 850px)');
+
+function verticalNav() {
+  section.style.display = "none";
+  navBar.style.position = "relative";
+  links.style.cssText = "height: 12vh; align-items: center; padding-bottom: 0";
+  verticalLinks.style.cssText = "display: flex; height: 88vh";
+}
+
+function navBarReturn() {
+  section.style.display = "block";
+  navBar.style.position = "fixed";
+  links.style.cssText = "padding-bottom: 3rem";
+  verticalLinks.style.cssText = "display: none";
+}
 
 barsIcon.addEventListener('click', (e => {
   if (section.style.display != 'none') {
@@ -13,16 +29,34 @@ barsIcon.addEventListener('click', (e => {
   }
 }));
 
-function verticalNav() {
-  section.style.display = "none";
-  navBar.style.position = "relative";
-  links.style.cssText = "height: 15vh";
-  verticalLinks.style.cssText = "display: flex; height: 85vh";
+function handleNavChange(e) {
+
+  if (verticalNavQuery.matches && section.style.display == 'none') {
+    navBarReturn();
+  }
+
 }
 
-function navBarReturn() {
-  section.style.display = "block";
-  navBar.style.position = "fixed";
-  links.style.cssText = "height: auto";
-  verticalLinks.style.cssText = "display: none";
+verticalNavQuery.addListener(handleNavChange);
+
+handleNavChange(verticalNavQuery);
+
+function handleIconChange(e) {
+
+  if (barsIconSizeQuery.matches) {
+    barsIcon.classList.remove('fa-4x');
+    barsIcon.classList.add('fa-3x');
+  } else {
+    barsIcon.classList.remove('fa-3x');
+    barsIcon.classList.add('fa-4x');
+  }
+
 }
+
+barsIconSizeQuery.addListener(handleIconChange);
+
+handleIconChange(barsIconSizeQuery);
+
+
+
+
