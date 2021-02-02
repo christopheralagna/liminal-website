92f2eacc6dbbecd9104d0625af41c80e5963a477
Christopher Alagna <christopheralagna1@gmail.com>
2021-02-01 22:36:54 -0700

    added google map API and formatted it in contact page. Set up contact form to work for netlify. Fixed font issue with contact form input and textarea fonts. Aligned h2 headers with their horizontal content.

--- scripts/main.js
+++ scripts/main.js
@@ -61,6 +61,34 @@ handleIconChange(barsIconSizeQuery);
 
 */
 
+/*
+
+function confirmSubmit(e) {
+  e.preventDefault();
+  let thankYou = document.createElement('div').innerHTML = "Thank You!";
+  let form = document.querySelector("form");
+  form.appendChild(thankYou);
+  console.log(thankYou);
+}
+
+*/
+
+
+function initMap() {
+  const headSpace = { lat: 35.19391220610381, lng: -111.64980189203136 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 18,
+    center: headSpace,
+    mapId: '1b35a3a4366c83c8'
+  });
+  const marker = new google.maps.Marker({
+    position: headSpace,
+    map: map,
+  });
+}
+
+
+
 
 
 
