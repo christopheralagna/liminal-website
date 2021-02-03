f871d9fa3efd61d631ba4e96328c26235d4ae09e
Christopher Alagna <christopheralagna1@gmail.com>
2021-02-03 12:43:18 -0700

    attempt to fix issue where vertical-nav is cut off on mobile. Changed main.js so to accomodate heights in percentages (%) rather than view heights (vh).

--- scripts/main.js
+++ scripts/main.js
@@ -4,19 +4,22 @@ const navBar = document.querySelector(".nav-bar");
 const links = document.querySelector(".links");
 const verticalLinks = document.querySelector('.vertical-links');
 const section = document.querySelector('section');
+const body = document.querySelector('body');
 const barsIconSizeQuery = window.matchMedia('(max-width: 500px)');
 const verticalNavQuery = window.matchMedia('(min-width: 850px)');
 
 function verticalNav() {
   section.style.display = "none";
-  navBar.style.position = "relative";
-  links.style.cssText = "height: 12vh; align-items: center; padding: 0";
-  verticalLinks.style.cssText = "display: flex; height: 88vh";
+  body.style.height = "100%";
+  navBar.style.cssText = "position: relative; height: 100%";
+  links.style.cssText = "height: 12%; align-items: center; padding: 0";
+  verticalLinks.style.cssText = "display: flex; height: 88%";
 }
 
 function navBarReturn() {
   section.style.display = "block";
-  navBar.style.position = "fixed";
+  body.style.height = "auto";
+  navBar.style.cssText = "position: fixed; height: auto";
   links.style.cssText = "padding: 3rem, auto";
   verticalLinks.style.cssText = "display: none";
 }
