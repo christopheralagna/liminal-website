9eab6c2d80ff862ff397bd1d40d0ead674300b15
Christopher Alagna <christopheralagna1@gmail.com>
2021-05-16 20:06:24 -0700

    added empty event messages, and formatted them

--- scripts/events.js
+++ scripts/events.js
@@ -7,17 +7,19 @@ const wideScreenQuery = window.matchMedia('(min-width: 1025px)');
 function highlightCurrentButton() {
   //currentButton.style.color = "var(--black-primary)";
   currentButton.style.color = "var(--black-secondary)";
+  //currentButton.style['font-weight'] = "600";
   //checks if loading page for first time
   if (currentButton.classList.contains("custom-hover")) {currentButton.classList.remove("custom-hover")} 
-  pastButton.style.color = "var(--black-tertiary)";
+  pastButton.style.color = "rgb(150,150,150)";
   pastButton.classList.add("custom-hover");
 }
 
 function highlightPastButton() {
   //pastButton.style.color = "var(--black-primary)";
   pastButton.style.color = "var(--black-secondary)";
+  //pastButton.style['font-weight'] = "600";
   pastButton.classList.remove("custom-hover");
-  currentButton.style.color = "var(--black-tertiary)";
+  currentButton.style.color = "rgb(150,150,150)";
   currentButton.classList.add("custom-hover");
 }
 
@@ -68,10 +70,14 @@ function pastEventsExist() {
   }
 }
 
-function createEmptyMessage() {
-  let emptyMessage = document.createElement("div");
+function createEmptyMessage(type) {
+  let emptyMessage = document.createElement("p");
   emptyMessage.classList.add("empty-message");
-  emptyMessage.innerHTML = "No Upcoming Events";
+  if (type == "current") {
+    emptyMessage.innerHTML = "No Upcoming Events";
+  } else if (type == "past") {
+    emptyMessage.innerHTML = "No Past Events";
+  }
   document.querySelector(".event-list").appendChild(emptyMessage);
 }
 
@@ -82,18 +88,11 @@ function removeEmptyMessage() {
 }
 
 function handleEmptyMessage(type) {
-  if (type == "current") {
-    if (!currentEventsExist()) {
-      createEmptyMessage();
-    } else {
-      removeEmptyMessage();
-    }
-  } else if (type == "past") {
-    if (!pastEventsExist()) {
-      createEmptyMessage();
-    } else {
-      removeEmptyMessage();
-    }
+  removeEmptyMessage();
+  if (type == "current" && !currentEventsExist()) {
+    createEmptyMessage("current");
+  } else if (type == "past" && !pastEventsExist()) {
+    createEmptyMessage("past");
   }
 }
 
